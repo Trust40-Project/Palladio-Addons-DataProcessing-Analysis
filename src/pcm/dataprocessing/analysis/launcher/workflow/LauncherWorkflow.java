77e6ca8b7f65234647fd0db075cb7414a85bea09
mirko <uwpmg>
2020-01-13 12:24:33 +0100

    Added testing implementation for Workflow

--- src/pcm/dataprocessing/analysis/launcher/workflow/LauncherWorkflow.java
+++ src/pcm/dataprocessing/analysis/launcher/workflow/LauncherWorkflow.java
@@ -5,15 +5,18 @@ import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.ConsoleAppender;
 
 import org.apache.log4j.PatternLayout;
+import org.eclipse.emf.common.util.URI;
 
 import de.uka.ipd.sdq.workflow.Workflow;
-
+import de.uka.ipd.sdq.workflow.blackboard.Blackboard;
 import de.uka.ipd.sdq.workflow.jobs.JobFailedException;
-
+import de.uka.ipd.sdq.workflow.jobs.ParallelJob;
 import de.uka.ipd.sdq.workflow.jobs.UserCanceledException;
+import pcm.dataprocessing.analysis.launcher.workflow.job.ModelResolverJob;
+import pcm.dataprocessing.analysis.launcher.workflow.job.PathResolverJob;
 
 public class LauncherWorkflow {
-	public static void main(String[] args) {
+	public void launch() {
 
 		// set up a basic logging configuration
 
@@ -21,11 +24,25 @@ public class LauncherWorkflow {
 
 		BasicConfigurator.configure(new ConsoleAppender(new PatternLayout("%m%n")));
 
-//		    HelloWorldJob job = new HelloWorldJob("World");
+		Blackboard<Object> myBlackboard = new Blackboard<Object>();
+		String pathKey = "path";
+		String modelKey = "model";
+		
+		myBlackboard.addPartition(modelKey, URI.createURI("test"));
+//		    PathResolver job = new PathResolver( input path);
 //
-		// Workflow myWorkflow = new Workflow(job);
+		ParallelJob parallelJob = new ParallelJob();
+		
+		// parallelJob.add(new PathResolverJob(input path))
+		
+		parallelJob.add(new ModelResolverJob(myBlackboard, modelKey));
+		
+		
+		Workflow myWorkflow = new Workflow(parallelJob);
+		myWorkflow.run();
+		
+		
 
-		// myWorkflow.run();
-
+		
 	}
 }
